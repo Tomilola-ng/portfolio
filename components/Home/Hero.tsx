d186699ef3eb79b3dc236adc1af17d884eb3787f
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2024-06-05 16:46:54 +0000

    ANIMATIONS

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,16 +1,31 @@
+"use client";
+
 import Image from "next/image";
 import myImage from "/public/tomilola.webp";
 import Link from "next/link";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function HeroSection() {
   return (
     <section className="maxContainer pSm my-8 flexCenter flex-col gap-y-6">
       <div className="w-full max-w-80 flexCenter flex-col gap-4 text-center text-pretty">
-        <Image
-          src={myImage}
-          alt="picture of tomilola"
-          className="rounded-full w-[200px]"
-        />
+        <AnimatePresence>
+          <motion.div
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 100 }}
+            transition={{
+              duration: 0.7,
+              ease: "easeIn",
+            }}
+            className=""
+          >
+            <Image
+              src={myImage}
+              alt="picture of tomilola"
+              className="rounded-full w-[200px]"
+            />
+          </motion.div>
+        </AnimatePresence>
         <h1 className="font-bold text-lg mb-0">Tomilola Oluwafemi</h1>
         <p className="text-sm">
           I am a certified software engineer with over{" "}
--- components/reusables/ProjectCard.tsx
+++ components/reusables/ProjectCard.tsx
@@ -1,17 +1,26 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 export default function ProjectCard({ project }: { project: any }) {
   return (
     <div
       key={project.link}
       className="flex flex-col shadow-sm bg-neutral-200 p-4 py-6 rounded group"
     >
-      <a
+      <motion.a
         href={`${project.link}`}
         target="_blank"
         rel="noreferrer"
+        whileHover={{
+          scale: [1, 1.2, 1.2, 1, 1],
+          rotate: [0, 0, 150, 150, 0],
+          borderRadius: ["10%", "20%", "50%", "50%", "50%"],
+        }}
         className="mb-4 aspect-square overflow-hidden flexCenter rounded relative"
       >
         <img src={project.photo} alt={project.title} />
-      </a>
+      </motion.a>
       <h3 className="text-black font-semibold">{project.title}</h3>
       <p className=" text-gray-800 mt-1 text-xs">{project.description}</p>
       <div className="mt-5">
